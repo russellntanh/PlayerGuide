5e2eb67f5d5ed55aabd37c682053772f2653f442
Russell Nguyen <russell.ntanh@gmail.com>
2025-09-29 05:59:38 +0700

    Record in c#

---
+++ RecordGeneric/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RecordGeneric
+{
+    // record class
+    public record Student(int Id, string Name, string Department);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instance by record
+            Student s1 = new Student(1000, "Russell", "CS");
+            Student s2 = new Student(1001, "Tom", "Law");
+
+            // decomposition
+            (var id, var name, var department) = s1;
+
+            // display
+            Console.WriteLine($"ID = {id}, Name: {name}, Department: {department}");
+
+            // comparison
+            Console.WriteLine(s1 == s2);  // false
+            Console.WriteLine(s1 == s1);  // true
+        }
+    }
+}
