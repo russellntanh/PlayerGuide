efe5bac9a8f8cbf3dc6b3051d640f7c9ef9f1f2e
Russell Nguyen <russell.nguyen@kohyoung.com>
2025-10-09 15:26:56 +0700

    Event without param in C#

---
+++ Advanced_Event/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+
+namespace Advanced_Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EVENT IN C#");
+
+            Ship ship = new Ship();
+            MessageManager showMess = new MessageManager(ship);
+            ship.Run();
+
+        }
+    }
+
+    // publisher
+    public class Ship
+    {
+        public event Action ShowMessage; // event without param
+
+        Random random = new Random();
+        public Point location { get;set;} = new Point();
+
+        public void Run()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"Ship's Blood amount is: " + i);
+
+                if (i == 0)
+                {
+                    ShowMessage.Invoke();
+                }
+            }
+        }
+    }
+
+    // subscriber
+    public class MessageManager
+    {
+        private void OnShowMessage() => ShowMessage("Exploded");
+
+        public MessageManager(Ship ship)
+        {
+            ship.ShowMessage += OnShowMessage;
+        }
+
+        public void ShowMessage(string messsage)
+        {
+            Console.WriteLine("Exploded.");
+        }
+
+    }
+}
