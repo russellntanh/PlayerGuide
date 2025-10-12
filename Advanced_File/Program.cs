c0e7383ecf34887f1c60a80d50073c79a9033037
Russell Nguyen <russell.ntanh@gmail.com>
2025-10-12 15:57:57 +0700

    Add Pattern Matching

---
+++ Advanced_File/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Advanced_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write and Read to/from File");
+
+            // Create data
+            List<Student> students = new List<Student>()
+            {
+                new Student("20250501", "Russell Nguyen", 80),
+                new Student("20250502", "Tom Pham", 90),
+                new Student("20250503", "Tony Nguyen", 50),
+                new Student("20250504", "Lamine Nguyen", 80),
+                new Student("20250505", "Jason Duong", 75),
+            };
+
+            // Convert data to string
+            string allStudents = "This file contains all students.";
+            string contents = String.Empty;
+
+            foreach (Student student in students)
+            {
+                contents += student.Id + "," + student.Name + "," + student.score + "\n";
+            }
+
+            // WriteAllFile
+            File.WriteAllText("D:\\students-write.txt", contents);
+
+
+            // ReadAllFile
+            string readFromFile = File.ReadAllText("D:\\students-read.txt");
+        }
+    }
+
+    public record Student(string Id, string Name, int score);
+
+}
---
+++ Advanced_PatternMatching/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Advanced_PatternMatching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PATTERN MATCHING");
+            
+            Console.WriteLine("Please enter a number(Exit by 0): ");
+
+            try
+            {
+                while (true)
+                {
+                    if(!int.TryParse(Console.ReadLine(), out int result))
+                    {
+                        Console.WriteLine("Please enter a number!!!");
+                        continue;
+                    }
+                    if (result == 0) break;
+
+                    Console.Write(" is " + MonthOfTheYear(result));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+        // Month of the Year
+        public static string MonthOfTheYear(int month)
+        {
+            return month switch
+            {
+                1 => "Jannuary",
+                2 => "February",
+                3 => "March",
+                4 => "April",
+                5 => "May",
+                6 => "June",
+                7 => "July",
+                8 => "August",
+                9 => "September",
+                10 => "October",
+                11 => "November",
+                12 => "December",
+                _ => "Not a month"
+            };
+        }
+    }
+
+
+}
