581828097b374987ca09a7af433ea0c4664f55e6
Russell Nguyen <russell.ntanh@gmail.com>
2025-10-31 06:25:26 +0700

    Add guess the number

---
+++ Advanced_Thread/LuckyNumberGuess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Thread
+{
+    public class LuckyNumberGuess
+    {
+        public int Previous { get; set; }
+        public int Current { get; set; }
+
+        private readonly Random _random = new Random();
+
+        private readonly object _lock = new object();
+
+        public LuckyNumberGuess()
+        {
+            Previous = -1;
+            Current = -1;
+        }
+
+        public void Update(int number)
+        {
+            lock(_lock)
+            {
+                Previous = Current;
+                Current = number;
+            }
+        }
+
+        public bool IsRepeat()
+        {
+            lock (_lock)
+            {
+                return Previous != -1 && Current != -1 && Previous == Current;
+            }
+        }
+
+        public int GetCurrent()
+        {
+            lock (_lock)
+            {
+                return Current;
+            }
+        }
+    }
+}
--- Advanced_Thread/Program.cs
+++ Advanced_Thread/Program.cs
@@ -3,34 +3,83 @@ using System.Threading;
 
 namespace Advanced_Thread
 {
-    public class ThreadWithLock
+    public class Program
     {
+
         public static void Main()
         {
-            GetEvenOddNumbers getEvenOddNumbers = new GetEvenOddNumbers();
-            Thread eventThread = new Thread(getEvenOddNumbers.GetEvenNumbers);
-            eventThread.Name = "Even Thread";
-            Thread oddThread = new Thread(getEvenOddNumbers.GetOddNumbers);
-            oddThread.Name = "Odd Thread";
-            
-            eventThread.Start();
-            oddThread.Start();
-            
-            eventThread.Join();
-            oddThread.Join();
+            Console.WriteLine("Hello, worl");
 
-            // Test thread with lock
-            SharedData sharedData = new SharedData();
-            Thread thread = new Thread(sharedData.Increate); // sub thread
-            thread.Start();
-            thread.Join();
-            Console.WriteLine("Sub thread: " + sharedData.Count);
+            Random random = new Random();
+            var luckyNumberGuess = new LuckyNumberGuess();
 
-            sharedData.Increate();
-            Console.WriteLine("Main thread: " +sharedData.Count);
+            // Thread to generate random number
+            Thread threadGenerator = new Thread(() =>
+            {
+                while (true)
+                {
+                    int number = random.Next(0, 10);
+                    luckyNumberGuess.Update(number);
+                    Console.WriteLine($"Generated number: {number}.");
+                    Thread.Sleep(1000);
+                }
+            });
+            //threadGenerator.IsBackground = true;
+
+
+            // Thread to guess number
+            Thread threadCheckRepeat = new Thread(() => 
+            {
+                Console.WriteLine("Press Enter to check repeated numbers");
+                Console.ReadLine();
+                while (true)
+                {
+                    if (luckyNumberGuess.IsRepeat())
+                    {
+                        Console.WriteLine($"Correct: {luckyNumberGuess.Previous}, {luckyNumberGuess.Current}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Not correct, 2 most recent numbers are different");
+                    }
+                    //Console.WriteLine("Press Enter to continue checking.");
+                }
+            });
+            //threadCheckRepeat.IsBackground = true;
+
+            threadGenerator.Start();
+            threadCheckRepeat.Start();
+
+            Console.WriteLine("Press Ctr + C to exit.");
+            Thread.Sleep(Timeout.Infinite); // Hoặc dùng Console.ReadLine() nếu muốn
+
         }
     }
 
+    //#region Test even odd number thread
+    //        GetEvenOddNumbers getEvenOddNumbers = new GetEvenOddNumbers();
+    //    Thread eventThread = new Thread(getEvenOddNumbers.GetEvenNumbers);
+    //        eventThread.Name = "Even Thread";
+    //        Thread oddThread = new Thread(getEvenOddNumbers.GetOddNumbers);
+    //oddThread.Name = "Odd Thread";
+            
+    //        eventThread.Start();
+    //        oddThread.Start();
+            
+    //        eventThread.Join();
+    //        oddThread.Join();
+    //        #endregion
+
+    //        #region Test thread with lock
+    //        SharedData sharedData = new SharedData();
+    //Thread thread = new Thread(sharedData.Increate); // sub thread
+    //thread.Start();
+    //        thread.Join();
+    //        Console.WriteLine("Sub thread: " + sharedData.Count);
+
+    //        sharedData.Increate();
+    //        Console.WriteLine("Main thread: " +sharedData.Count);
+    //        #endregion
     public class SharedData
     {
         private readonly object _lockNumber = new();
